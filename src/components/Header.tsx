d3a8855ff66a516ff3d622482f08d609a325fc2f
Shaoe <pz.zoli@gmail.com>
2024-01-09 22:17:35 +0100

    styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,13 @@ const Header = () => (
   <header className="h-20 bg-base-200 grid place-items-center ">
     <div role="tablist" className="tabs flex w-3/5">
       <a role="tab" className="tab tab-lg flex-auto ">
-        <article className="prose prose-stone prose-xl">Domov</article>
+        <p className="prose prose-stone prose-xl font-bold">Domov</p>
       </a>
       <a role="tab" className="tab tab-lg flex-auto">
-        <article className="prose prose-stone prose-xl">OZ sklenik</article>
+        <p className="prose prose-stone prose-xl font-bold">OZ sklenik</p>
       </a>
       <a role="tab" className="tab tab-lg flex-auto">
-        <article className="prose prose-stone prose-xl">Galeria</article>
+        <p className="prose prose-stone prose-xl font-bold">Galeria</p>
       </a>
     </div>
   </header>
