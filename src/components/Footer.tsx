33baee26779034fb2a0f68e79582939e0e47b5f9
Shaoe <pz.zoli@gmail.com>
2024-01-09 23:13:19 +0100

    added text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => (
   <div className="h-10">
     <footer className="footer footer-center p-8  bg-base-200 text-base-content ">
-      <p className="prose prose-stone prose-xl font-bold">
+      <p className="prose  prose-xl font-bold">
         Kontakt
         <a
           href={"mailto:oz.skleniky@gmail.com"}
@@ -23,13 +23,75 @@ const Footer = () => (
       </a>
     </footer>
     <footer className="footer footer-center p-5 bg-base-200 text-base-content border-t border-base-300">
-      <p className="prose prose-stone prose-xl font-bold">Podporili nás</p>
+      <p className="prose  prose-xl font-bold">Podporili nás</p>
 
       <div className="grid grid-flow-col gap-4 ">
         <img src="BAlogo_centr_black.svg" className="w-2/3 max-w-xl" />
         <img src="logoAKAop.png" className="w-1/3 max-w-xl" />
       </div>
     </footer>
+    <article className="prose max-w-full p-10 bg-base-200 text-base-content border-t border-base-300">
+      <h3>Čo je skleník?</h3>
+      <p>
+        Je združenie ľudí, ktorí sa snažia o revitalizáciu pôvodného objektu
+        skleníka z roku 1978 v rámci bývalého poľnohospodárskeho areálu v
+        Bratislave. Za týmto účelom bolo v roku 2022 založené občianske
+        združenie OZ skleníky, aby sa spoločne jeho členom a priateľom podarilo
+        naplniť sebestačnosť produkcie potravín a myšlienky trvalo udržateľného
+        rozvoja v mestskom prostredí.
+      </p>
+      <h3>Ciele združenia:</h3>
+      <ul>
+        <li>ochraňovať životné prostredie.</li>
+        <li>
+          aktivity na podporu trvalo udržateľného rozvoja v mestskom prostredí,
+          v rámci sebestačnosti produkcie potravín a nevyhnutných potrieb pre
+          život a existenciu.
+        </li>
+        <li>
+          aktivity na podporu vybudovania pilotného komunitného centra pre
+          trvalo udržateľný rozvoj s využitím ekologicky obnoviteľných zdrojov a
+          technologických možností so zameraním na zníženie dopadov vzniku
+          globálneho otepľovania a zmeny klímy.
+        </li>
+        <li>
+          oslovovanie štátnych, regionálnych, mestských zastupiteľov a
+          inštitúcií za účelom pomoci pri presadzovaní trvalo udržateľného
+          rozvoja, využívania ekologických konceptov, recyklácie materiálov,
+          potravinovej sebestačnosti a ľudských zdrojov.
+        </li>
+        <li>
+          aktivity vytvorenia legislatívy a podpory týkajúce sa využívania
+          obnoviteľných, ekologických zdrojov energie, produkcie potravín a
+          bezuhlíkovej mobility pre existenciu obyvateľov v rámci urbanistickej
+          štruktúry mesta.
+        </li>
+        <li>
+          propagácia formou tlačových materiálov (vydávanie letákov, brožúr,
+          newsletterov), tlačových správ, webu, audio a video materiálov.
+        </li>
+        <li>tvorba propagačných materiálov (video, audio, tlačových, web).</li>
+        <li>
+          práca s verejnosťou a so znevýhodnenými skupinami obyvateľstva
+          zameraná na osvetu, propagácia ekologických a experimentálnych
+          možností získavania energie, šírenie informácií o dostupných
+          technológiách, možnostiach recyklácie a udržateľnosti, produkcia
+          vlastných potravín, potravinová banka, freefood.
+        </li>
+        <li>vzdelávanie o trvalo udržateľnej agrokultúre.</li>
+        <li>
+          podpora a organizovanie spoločenských, umeleckých, kultúrnych,
+          športových a iných podujatí, prednášok, školení, seminárov,
+          workshopov, vzdelávacích programov, projekcií, kongresov, koncertov,
+          networkingových a benefičných akcii. Podpora malého podnikania formou
+          poskytovania priestorov, ľudských zdrojov a dobrovoľných príspevkov.
+        </li>
+        <li>
+          organizovanie študijných pobytov svojich členov v zahraničí ako aj
+          členov partnerských organizácii.
+        </li>
+      </ul>
+    </article>
   </div>
 );
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,13 @@ const Header = () => (
   <header className="h-20 bg-base-200 grid place-items-center ">
     <div role="tablist" className="tabs flex w-3/5">
       <a role="tab" className="tab tab-lg flex-auto ">
-        <p className="prose prose-stone prose-xl font-bold">Domov</p>
+        <p className="prose  prose-xl font-bold">Domov</p>
       </a>
       <a role="tab" className="tab tab-lg flex-auto">
-        <p className="prose prose-stone prose-xl font-bold">OZ skleníky</p>
+        <p className="prose  prose-xl font-bold">OZ skleníky</p>
       </a>
       <a role="tab" className="tab tab-lg flex-auto">
-        <p className="prose prose-stone prose-xl font-bold">Galéria</p>
+        <p className="prose  prose-xl font-bold">Galéria</p>
       </a>
     </div>
   </header>
