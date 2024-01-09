c9defc85a734916a4629fe7fb4b9a79149ffd453
Shaoe <pz.zoli@gmail.com>
2024-01-09 23:23:32 +0100

    header responsive size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,14 @@
 const Header = () => (
   <header className="h-20 bg-base-200 grid place-items-center ">
-    <div role="tablist" className="tabs flex w-3/5">
-      <a role="tab" className="tab tab-lg flex-auto ">
-        <p className="prose  prose-xl font-bold">Domov</p>
+    <div role="tablist" className="tabs grid grid-flow-col gap-4">
+      <a role="tab" className="tab  flex-auto ">
+        <p className="prose  lg:prose-xl font-bold">Domov</p>
       </a>
-      <a role="tab" className="tab tab-lg flex-auto">
-        <p className="prose  prose-xl font-bold">OZ skleníky</p>
+      <a role="tab" className="tab  flex-auto">
+        <p className="prose  lg:prose-xl font-bold">OZ skleníky</p>
       </a>
-      <a role="tab" className="tab tab-lg flex-auto">
-        <p className="prose  prose-xl font-bold">Galéria</p>
+      <a role="tab" className="tab  flex-auto">
+        <p className="prose  lg:prose-xl font-bold">Galéria</p>
       </a>
     </div>
   </header>
