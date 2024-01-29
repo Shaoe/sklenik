e0905c6bcdc8e15fef51b442462d82f3bee3c1a4
Shaoe <pz.zoli@gmail.com>
2024-01-29 13:01:00 +0100

    timer on carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,47 +1,53 @@
+import { useEffect, useState } from "react";
 import img1 from "../assets/1_logo.jpg";
 import img2 from "../assets/2_logo.jpg";
 import img3 from "../assets/3_logo.jpg";
 // import sklenikLogo from "../assets/logo_final.png";
 
-const Carousel = () => (
-  <div className="carousel w-full">
-    <div id="slide1" className="carousel-item relative w-full">
-      {/* <div className="relative"> */}
-      <img src={img1} className="w-full" />
-      {/* <img src={sklenikLogo} className="w-1/3 lg:w-1/5" /> */}
-      {/* </div> */}
-      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide3" className="btn btn-circle">
-          ❮
-        </a>
-        <a href="#slide2" className="btn btn-circle">
-          ❯
-        </a>
+const Carousel = () => {
+  const timerCD = 5000;
+  const [state, setState] = useState(1);
+  const [mousedOver, setMousedOver] = useState(false);
+
+  useEffect(() => {
+    const timerId = setTimeout(
+      () =>
+        setState((old) => {
+          if (old === 3) return 1;
+          else return old + 1;
+        }),
+      timerCD
+    );
+    console.log(state);
+    if (mousedOver) return () => clearTimeout(timerId);
+
+    const div = document.getElementById("carousel");
+    div?.scrollTo({
+      left: document.getElementById(`slide${state}`)?.offsetLeft,
+    });
+  }, [state, mousedOver]);
+
+  return (
+    <div
+      id="carousel"
+      className="carousel w-full"
+      onMouseOver={() => setMousedOver(true)}
+      onMouseOut={() => setMousedOver(false)}
+    >
+      <div id="slide1" className="carousel-item relative w-full">
+        {/* <div className="relative"> */}
+        <img src={img1} className="w-full" />
+        {/* <img src={sklenikLogo} className="w-1/3 lg:w-1/5" /> */}
+        {/* </div> */}
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img src={img2} className="w-full" />
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img src={img3} className="w-full" />
       </div>
     </div>
-    <div id="slide2" className="carousel-item relative w-full">
-      <img src={img2} className="w-full" />
-      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide1" className="btn btn-circle">
-          ❮
-        </a>
-        <a href="#slide3" className="btn btn-circle">
-          ❯
-        </a>
-      </div>
-    </div>
-    <div id="slide3" className="carousel-item relative w-full">
-      <img src={img3} className="w-full" />
-      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide2" className="btn btn-circle">
-          ❮
-        </a>
-        <a href="#slide1" className="btn btn-circle">
-          ❯
-        </a>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Carousel;
