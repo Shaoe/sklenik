f7e2d0c0cb17ea6db25b59ccb34b6dce511ad9b7
Shaoe <pz.zoli@gmail.com>
2024-02-10 13:12:58 +0100

    carousel fade in fade out

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Carousel from "./components/Carousel";
-// import BottomLine from "./components/BottomLine";
+import Carousel from "./components/Carousel/Carousel";
+import BottomLine from "./components/BottomLine";
+import LayeredImage from "./components/LayeredImage";
+
+import img1 from "./assets/1.jpg";
+import logo from "./assets/logo_final_invert.png";
+import img2 from "./assets/2.jpg";
+import img3 from "./assets/3.jpg";
+// import sklenikLogo from "../assets/logo_final.png";
 
 function App() {
   return (
-    <div className="flex flex-col h-screen justify-between ">
-      <div className="min-h-full max-h-full ">
-        <Header />
-        <div className="mb-auto pl-40 pr-40 ">
-          <Carousel />
+    <div className="">
+      <div className="h-screen flex flex-col">
+        <div className="">
+          <Header />
         </div>
-        {/* <BottomLine /> */}
+        <div className="mb-auto pl-40 pr-40 grow">
+          <Carousel>
+            <LayeredImage img={img1} overlay={logo} />
+            <LayeredImage img={img2} overlay={logo} />
+            <LayeredImage img={img3} overlay={logo} />
+          </Carousel>
+        </div>
+        {/* <div className="flex-none h-20 w-full bg-base-200 grid place-items-center  absolute bottom-0  z-30">
+          logo
+        </div> */}
+
+        {/* <div className="">
+          <BottomLine />
+        </div> */}
       </div>
       <Footer />
     </div>
---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,42 @@
+import { ReactNode, useEffect, useState } from "react";
+import "./Carousel.css";
+
+interface Props {
+  children: ReactNode[];
+}
+
+const Carousel = (props: Props) => {
+  const content = props.children;
+  const timerCD = 3000;
+  const [counter, setCounter] = useState(1);
+  const [mousedOver, setMousedOver] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!mousedOver) {
+        handleNext();
+      } else {
+        clearInterval(interval);
+      }
+    }, timerCD);
+    return () => clearInterval(interval);
+  });
+
+  const handleNext = () => {
+    if (counter !== content.length) {
+      setCounter(counter + 1);
+    } else {
+      setCounter(1);
+    }
+  };
+
+  const activeSlide = content.map((item, index) => (
+    <div className={counter - 1 === index ? "show" : "not-show"} key={index}>
+      {item}
+    </div>
+  ));
+
+  return <div>{activeSlide}</div>;
+};
+
+export default Carousel;
