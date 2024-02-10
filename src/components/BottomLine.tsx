61025d246a4674f5753acbedffc571f1f6839b7b
Shaoe <pz.zoli@gmail.com>
2024-02-10 19:43:45 +0100

    soc media on the bottom

--- src/components/BottomLine.tsx
+++ src/components/BottomLine.tsx
@@ -1,5 +1,15 @@
+import fbLogo from "../assets/logo_facebook.png";
+import instaLogo from "../assets/logo_instagram.png";
+
 const BottomLine = () => (
-  <div className="h-20  bg-base-200 grid place-items-center">logo</div>
+  <div className="h-20  bg-base-200 flex flex-row	items-center justify-center">
+    <a href={"https://www.facebook.com/profile.php?id=100091756742140"}>
+      <img src={fbLogo} className="max-h-12" />
+    </a>
+    {/* <a href={""}> */}
+    <img src={instaLogo} className="max-h-12" />
+    {/* </a> */}
+  </div>
 );
 
 export default BottomLine;
