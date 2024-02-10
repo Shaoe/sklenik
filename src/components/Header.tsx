1d5e8f700decc400c8dc6d9d201df7e74cd9305f
Shaoe <pz.zoli@gmail.com>
2024-02-10 21:01:09 +0100

    menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,16 @@
 import logo from "../assets/logo_final.png";
+import Menu from "./menu/Menu";
 
 const Header = () => (
-  <header className="h-20 bg-base-0 grid grid-cols-2 place-items-center">
+  <header className="h-20 grid grid-cols-2 place-items-center">
     <div className="flex">
       <a className="absolute left-5 top-0 flex justify-center items-center flex-nowrap p-4 bg-base-0">
-        <img src={logo} className="max-h-12 hidden lg:block" />
+        <img src={logo} className="max-h-12 lg:block" />
       </a>
     </div>
     <div
       role="tablist"
-      className="tabs grid grid-flow-col gap-2 lg:gap-40 absolute"
+      className="tabs grid grid-flow-col lg:gap-40 absolute invisible lg:visible"
     >
       <a href="#sklenik_description" role="tab" className="tab">
         <p className="font-bold">OZ SKLENÍKY</p>
@@ -20,6 +21,11 @@ const Header = () => (
       <a href="#kontakt" role="tab" className="tab">
         <p className=" font-bold">KONTAKT</p>
       </a>
+    </div>
+    <div className="visible lg:invisible">
+      <div className="absolute right-2 top-3">
+        <Menu />
+      </div>
     </div>
   </header>
 );
---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,46 @@
+import { Dropdown } from "react-daisyui";
+
+const Menu = () => {
+  return (
+    <>
+      <Dropdown horizontal="left" vertical="bottom">
+        <Dropdown.Toggle button={false}>
+          <button className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-6 h-6 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+        </Dropdown.Toggle>
+        <Dropdown.Menu className="w-52 mt-4 z-30">
+          <Dropdown.Item>
+            <a href="#sklenik_description" role="tab" className="tab">
+              <p className="font-bold">OZ SKLENÍKY</p>
+            </a>
+          </Dropdown.Item>
+          <Dropdown.Item>
+            <a href="#galeria" role="tab" className="tab">
+              <p className="font-bold">FOTOGALÉRIA</p>
+            </a>
+          </Dropdown.Item>
+          <Dropdown.Item>
+            <a href="#kontakt" role="tab" className="tab">
+              <p className=" font-bold">KONTAKT</p>
+            </a>
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </>
+  );
+};
+
+export default Menu;
