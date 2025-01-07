397ec0dcfd7fe79786e3bf38cf32086d51313a32
Shaoe <pz.zoli@gmail.com>
2025-01-07 20:28:21 +0100

    formatting

--- src/components/Galery.tsx
+++ src/components/Galery.tsx
@@ -1,17 +1,16 @@
 import YearPhotoAlbum from "./YearPhotoAlbum";
 
 const Galery = () => {
-
   return (
     <div id="galeria" className="space-y-6">
       <p className="font-bold">FOTOGALÉRIA</p>
 
-        <YearPhotoAlbum year={2021} />
+      <YearPhotoAlbum year={2021} />
 
-        <YearPhotoAlbum year={2022} />
+      <YearPhotoAlbum year={2022} />
 
-        <YearPhotoAlbum year={2023} />
-      <div/>
+      <YearPhotoAlbum year={2023} />
+      <div />
     </div>
   );
 };
--- src/components/YearPhotoAlbum.tsx
+++ src/components/YearPhotoAlbum.tsx
@@ -5,133 +5,152 @@ import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
 import PhotoAlbum from "react-photo-album";
 
-
 import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 import "react-photo-album/styles.css";
 
-
-
 interface Props {
   year: number;
 }
 
-const YearPhotoAlbum = ({year}: Props) => {
-  const thumbnailUrls = importThumbnailsGlob(year)
-  const imgUrls = importImagesGlob(year)
+const YearPhotoAlbum = ({ year }: Props) => {
+  const thumbnailUrls = importThumbnailsGlob(year);
+  const imgUrls = importImagesGlob(year);
 
   const [thumbnails, updateData] = useState<any>([]);
   useEffect(() => {
-    const imgSrcPromises = thumbnailUrls.map(async src => {
-      const dimensions : any = await getHeightAndWidthFromDataUrl(src)
+    const imgSrcPromises = thumbnailUrls.map(async (src) => {
+      const dimensions: any = await getHeightAndWidthFromDataUrl(src);
       return {
         src,
         width: dimensions?.width,
-        height: dimensions?.height
-      }
-    })
-    Promise.all(imgSrcPromises).then(data => { updateData(data) })
+        height: dimensions?.height,
+      };
+    });
+    Promise.all(imgSrcPromises).then((data) => {
+      updateData(data);
+    });
   }, []);
 
-
- const slides = imgUrls.map((src) => ({
+  const slides = imgUrls.map((src) => ({
     src,
     width: 1980,
-    height: 1320 
-  }))
+    height: 1320,
+  }));
 
-  const [index, setIndex] = React.useState(-1);  
-  return (thumbnails &&
-    
-    <div id="album" className="">
-      <Lightbox
-        index={index}
-        slides={slides}
-        plugins={[Fullscreen, Thumbnails]}
-        open={index >= 0}
-        close={() => setIndex(-1)}
-      />
-      <div className="grid grid-cols-2">
-      <p className="font-bold lg:pt-5">{year}</p>
-        <PhotoAlbum photos={thumbnails} 
-         layout="rows"
-        //  columns={10}
-        targetRowHeight={66}
-          spacing={2}
-          padding={0}
-          onClick={({ index: current }) => setIndex(current)}/>
+  const [index, setIndex] = React.useState(-1);
+  return (
+    thumbnails && (
+      <div id="album" className="">
+        <Lightbox
+          index={index}
+          slides={slides}
+          plugins={[Fullscreen, Thumbnails]}
+          open={index >= 0}
+          close={() => setIndex(-1)}
+        />
+        <div className="grid grid-cols-2">
+          <p className="font-bold lg:pt-5">{year}</p>
+          <PhotoAlbum
+            photos={thumbnails}
+            layout="rows"
+            //  columns={10}
+            targetRowHeight={66}
+            spacing={2}
+            padding={0}
+            onClick={({ index: current }) => setIndex(current)}
+          />
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
 export default YearPhotoAlbum;
 
-const getHeightAndWidthFromDataUrl = (dataURL: string) => new Promise(resolve => {
-  const img = new Image()
-  img.onload = () => {
-    resolve({
-      height: img.height,
-      width: img.width
-    })
-  }
-  img.src = dataURL
-})
+const getHeightAndWidthFromDataUrl = (dataURL: string) =>
+  new Promise((resolve) => {
+    const img = new Image();
+    img.onload = () => {
+      resolve({
+        height: img.height,
+        width: img.width,
+      });
+    };
+    img.src = dataURL;
+  });
 
 const importThumbnailsGlob = (year: number) => {
-switch (year) {
-  case 2021:
-   return Object.values(
-      import.meta.glob(`/src/assets/2021/small/thumbnail/*.{png,jpg,JPG,jpeg,PNG,JPEG}`, {
-        eager: true,
-        as: "url",
-      })
-    );
-  case 2022:
-   return Object.values(
-      import.meta.glob(`/src/assets/2022/small/thumbnail/*.{png,jpg,JPG,jpeg,PNG,JPEG}`, {
-        eager: true,
-        as: "url",
-      })
-    );
-  case 2023:
-      return Object.values(
-         import.meta.glob(`/src/assets/2023/small/thumbnail/*.{png,jpg,JPG,jpeg,PNG,JPEG}`, {
-           eager: true,
-           as: "url",
-         })
-       );
-  default:
-    return [];
-}
-}
-
-const importImagesGlob = (year: number) => {
   switch (year) {
     case 2021:
-     return Object.values(
-        import.meta.glob(`/src/assets/2021/small/*.{png,jpg,JPG,jpeg,PNG,JPEG}`, {
-          eager: true,
-          as: "url",
-        })
+      return Object.values(
+        import.meta.glob(
+          `/src/assets/2021/small/thumbnail/*.{png,jpg,JPG,jpeg,PNG,JPEG}`,
+          {
+            eager: true,
+            as: "url",
+          }
+        )
       );
     case 2022:
       return Object.values(
-          import.meta.glob(`/src/assets/2022/small/*.{png,jpg,JPG,jpeg,PNG,JPEG}`, {
+        import.meta.glob(
+          `/src/assets/2022/small/thumbnail/*.{png,jpg,JPG,jpeg,PNG,JPEG}`,
+          {
             eager: true,
             as: "url",
-          })
-        );
+          }
+        )
+      );
     case 2023:
-    return Object.values(
-        import.meta.glob(`/src/assets/2023/small/*.{png,jpg,JPG,jpeg,PNG,JPEG}`, {
-          eager: true,
-          as: "url",
-        })
+      return Object.values(
+        import.meta.glob(
+          `/src/assets/2023/small/thumbnail/*.{png,jpg,JPG,jpeg,PNG,JPEG}`,
+          {
+            eager: true,
+            as: "url",
+          }
+        )
       );
     default:
       return [];
   }
+};
 
-}
+const importImagesGlob = (year: number) => {
+  switch (year) {
+    case 2021:
+      return Object.values(
+        import.meta.glob(
+          `/src/assets/2021/small/*.{png,jpg,JPG,jpeg,PNG,JPEG}`,
+          {
+            eager: true,
+            as: "url",
+          }
+        )
+      );
+    case 2022:
+      return Object.values(
+        import.meta.glob(
+          `/src/assets/2022/small/*.{png,jpg,JPG,jpeg,PNG,JPEG}`,
+          {
+            eager: true,
+            as: "url",
+          }
+        )
+      );
+    case 2023:
+      return Object.values(
+        import.meta.glob(
+          `/src/assets/2023/small/*.{png,jpg,JPG,jpeg,PNG,JPEG}`,
+          {
+            eager: true,
+            as: "url",
+          }
+        )
+      );
+    default:
+      return [];
+  }
+};
