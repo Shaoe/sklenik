c2d31e521091fb5bd2d5c5eba4745fec12480849
Shaoe <pz.zoli@gmail.com>
2026-06-16 00:04:41 +0200

    ES2022

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  build: {
+    target: "es2022",
+  },
+  esbuild: {
+    supported: {
+      destructuring: true,
+    },
+  },
 });
